00835e33d16aa8bc5a0a09a1d988beb29abf0a59
Peeyush609 <peeyushgupta609@gmail.com>
2025-08-20 14:58:37 +0530

    esy

---
+++ gfg61.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == x)  return true;
+            }
+        }
+        
+        return false;
+    }
+};
