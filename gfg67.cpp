971fe977e961ca5427dc0a63529bd0d6ed38eec4
Peeyush609 <peeyushgupta609@gmail.com>
2025-08-26 11:00:08 +0530

    easy

---
+++ gfg67.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    bool isSubSeq(string& s1, string& s2) {
+        // code here
+        int n = s1.size();
+        int m = s2.size();
+        int i = 0; 
+        int j = 0;
+        
+        while(j < m){
+            if(s1[i] == s2[j]){
+                i++;
+            }
+            j++;
+        }
+        
+        return i == n;
+    }
+};
