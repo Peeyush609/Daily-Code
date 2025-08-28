c3816f8b978a5d09e1ece2dfaea602811fece010
Peeyush609 <peeyushgupta609@gmail.com>
2025-08-28 10:32:59 +0530

    sliding window

---
+++ gfg69.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int maxOnes(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        
+        int ans = k;
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        
+        while(j < n){
+            
+            if(arr[j] == 0) count++;
+            
+            while(count > k){
+                if(arr[i] == 0) count--;
+                i++;
+            }
+            
+            ans = max(ans, j - i + 1);
+            j++;
+        }
+        
+        return ans;
+    }
+};
