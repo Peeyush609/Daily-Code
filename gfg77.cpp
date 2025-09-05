415fd1d0e1909ef9f3250cc1a46cb3e25b1b9782
Peeyush609 <peeyushgupta609@gmail.com>
2025-09-05 16:14:24 +0530

    separate 0 1 2 in linked list

---
+++ gfg77.cpp
@@ -0,0 +1,58 @@
+/*
+
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    // Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node* head) {
+        
+        // Add code here
+        
+        int zeros=0;
+        int ones=0;
+        int twos=0;
+        
+        Node* temp=head;
+        
+        while(temp){
+            if(temp->data==0)   zeros++;
+            else if(temp->data==1)  ones++;
+            else    twos++;
+            
+            temp=temp->next;
+            
+        }
+        
+        temp=head;
+        
+        while(zeros){
+            temp->data=0;
+            temp=temp->next;
+            zeros--;
+        }
+        while(ones){
+            temp->data=1;
+            temp=temp->next;
+            ones--;
+        }
+        while(twos){
+            temp->data=2;
+            temp=temp->next;
+            twos--;
+        }
+        
+        return head;
+        
+    }
+};
+
