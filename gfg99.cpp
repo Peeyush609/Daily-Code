2de1ff1f79bfadca1402d56beebb66d13cf0513c
Peeyush609 <peeyushgupta609@gmail.com>
2025-09-26 17:06:04 +0530

    easy dequeue

---
+++ gfg99.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void rotateDeque(deque<int>& dq, int type, int k) {
+        // code here
+        if(type == 1){
+            while(k--){
+                int x = dq.back();
+                dq.pop_back();
+                dq.push_front(x);
+            }
+        }
+        else{
+            while(k--){
+                int x = dq.front();
+                dq.pop_front();
+                dq.push_back(x);
+            }
+        }
+    }
+};
