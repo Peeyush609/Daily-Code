950849ed2852f03337ca746faa6124f5f08b7697
Peeyush609 <peeyushgupta609@gmail.com>
2025-09-27 23:30:25 +0530

    easy revist

---
+++ lc103.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        int N = points.size();
+        double ans = 0.0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    ans = max(ans, area(points[i], points[j], points[k]));
+                }
+            }
+        }
+        return ans;
+    }
+
+private:
+    double area(const vector<int>& P, const vector<int>& Q, const vector<int>& R) {
+        return 0.5 * abs(P[0]*Q[1] + Q[0]*R[1] + R[0]*P[1]
+                       - P[1]*Q[0] - Q[1]*R[0] - R[1]*P[0]);
+    }
+};
+
