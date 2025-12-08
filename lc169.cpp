a7e7177fb4339a1b5527f9db8dba2b9bae14c3d0
Peeyush609 <peeyushgupta609@gmail.com>
2025-12-08 10:28:53 +0530

    easyy

---
+++ lc169.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int diff = high - low + 1;
+
+        if(diff%2 == 0){
+            return diff/2;
+        }
+        else{
+            if(low % 2==0){
+                return (diff -1)/2;
+            }
+            else{
+                return (diff+1)/2;
+            }
+        }
+        return -1;
+    }
+};
