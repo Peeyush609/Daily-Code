d205ad78edbf569463991bb79087a3c53178f1f8
Peeyush609 <peeyushgupta609@gmail.com>
2026-01-12 13:04:13 +0530

    esuu

---
+++ lc204.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans = 0;
+
+        for(int i = 0 ; i < n - 1; i++){
+            int val = max(abs(points[i + 1][0] - points[i][0]), abs(points[i + 1][1] - points[i][1]));
+            ans += val;
+        }
+
+        return ans;
+    }
+};
