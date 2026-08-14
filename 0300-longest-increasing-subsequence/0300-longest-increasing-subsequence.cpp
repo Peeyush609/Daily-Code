36ef925664c12575d72c4d3ad12a5557bfd741f1
Peeyush Gupta <144371569+Peeyush609@users.noreply.github.com>
2026-08-14 15:26:38 +0530

    Time: 1 ms (81.33%), Space: 14.1 MB (87.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& nums, int n, int last, vector<vector<int>>& dp){
-        if(i == n)  return 0;
-
-        if(dp[i][last] != -1)   return dp[i][last];
-
-        int take = 0, skip = 0;
-        skip = solve(i + 1, nums, n, last, dp);
-
-        if(nums[i] > nums[last]){
-            take = 1 + solve(i + 1, nums, n, i, dp);
-        }
-
-        return dp[i][last] = max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int ans = 1;
-        for(int i = 0; i < n; i++){
-            ans = max(ans, 1 + solve(i + 1, nums, n, i, dp));
+        vector<int> temp;
+        for(int x: nums){
+            if(temp.empty() || temp[temp.size() - 1] < x){
+                temp.push_back(x);
+            }
+            else{
+                // frind the index of the smallest element greater than equal to x
+                auto it = lower_bound(temp.begin(), temp.end(), x);
+                *it = x;
+            }
         }
-        return ans;
+
+        return temp.size();
+
     }
 };
