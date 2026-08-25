feaa07d89b24cea51172092916df84fec1b0f1d2
Peeyush Gupta <144371569+Peeyush609@users.noreply.github.com>
2026-08-25 12:11:23 +0530

    Time: 5 ms (15.61%), Space: 25.2 MB (24.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int n = nums.size();
-        int ans = 0;
-
         unordered_set<int> ss(nums.begin(), nums.end());
-
         int val = k;
         while(ss.count(val)){
             val += k;
         }
-
         return val;
     }
 };
