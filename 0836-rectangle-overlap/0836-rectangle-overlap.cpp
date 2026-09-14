d2c345f905607330e981eec8564a21895a99fcc9
Peeyush Gupta <144371569+Peeyush609@users.noreply.github.com>
2026-09-14 10:44:21 +0530

    Time: 0 ms (100%), Space: 9.9 MB (47.04%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        // Check if width and height of intersection are both positive
+        int x_overlap = min(rec1[2], rec2[2]) - max(rec1[0], rec2[0]);
+        int y_overlap = min(rec1[3], rec2[3]) - max(rec1[1], rec2[1]);
+        
+        return x_overlap > 0 && y_overlap > 0;
+    }
+};
